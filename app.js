a7edc5ba3a2686412ba806f836c77f844a5e8077
Villy Siu <villysiu@gmail.com>
2026-02-02 22:04:34 -0800

    added cors, swagger

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+
 const express = require('express');
 const app = express();
-
 
 const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication');
 
+// routers
 const authRouter = require('./routes/auth');
 const menuitemRouter = require('./routes/menuitems');
 const milkRouter = require('./routes/milks');
@@ -19,19 +32,34 @@ const ordersRouter = require('./routes/orders');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+
 app.use(express.json());
-// extra packages
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 
 // routes
 app.get('/', (req, res) => {
-  res.send('Tea api');
+  res.send('<h1>Tea API</h1><a href="/api-docs">Documentation</a>');
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/menuitems', menuitemRouter)
 app.use('/api/v1/milks', milkRouter)
 app.use('/api/v1/sizes', sizeRouter)
 app.use('/api/v1/categories', categoryRouter)
-app.use('/api/v1/orders', authenticateUser, ordersRouter)
+// app.use('/api/v1/orders', authenticateUser, ordersRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -42,8 +70,6 @@ const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
-      // connect DB
-      
       console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
--- routes/auth.js
+++ routes/auth.js
@@ -5,4 +5,5 @@ const { register, login } = require('../controllers/auth')
 router.post('/register', register)
 router.post('/login', login)
 
+
 module.exports = router
