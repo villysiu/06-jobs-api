ddc194ca035311f083efbd0d861ce576c388b3c2
Villy Siu <villysiu@gmail.com>
2026-02-08 10:31:05 -0800

    updated message

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,13 +1,15 @@
 const isAdmin = async (req, res, next) => {
-    console.log("check user is admin")
-    console.log(req.user)
-    console.log(req.user.role)
+    // console.log("check user is admin")
+    // console.log(req.user)
+    // console.log(req.user.role)
+
+    // chaeck if user login
     if(!req.user)
         return res.status(401).json({message: "Unauthorized"})
     
-
+// check user role
     if(req.user.role !== "ADMIN")
-        return res.status(403).json({messag: "Forbidden"})
+        return res.status(403).json({message: "Not authorzied. Only Admin role allowed."})
 
     next();
 }
